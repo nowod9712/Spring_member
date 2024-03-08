e47d1e85a81ee7612927b6ceabc13e11c8d77590
nowod <ldowon1223@naver.com>
2024-03-08 19:41:53 +0900

    회원가입 기능 구현

--- src/main/java/com/study/erum/controller/MemberController.java
+++ src/main/java/com/study/erum/controller/MemberController.java
@@ -7,11 +7,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.study.erum.dto.MemberDTO;
+import com.study.erum.service.MemberService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("/member")//모든 핸들러 메서드가 /member로 시작하는 URL에 매핑된다.
+@RequiredArgsConstructor
 public class MemberController {
 
+	private final MemberService memberService;
 	
 	@GetMapping("/save")
 	public String saveForm() {
@@ -21,7 +26,12 @@ public class MemberController {
 	//주의 사항 : DTO 변수와 save.jsp에 있는 name이 반드시 일치해야 @ModelAttribue를 사용 가능!
 	@PostMapping("/save")
 	public String save(@ModelAttribute MemberDTO memberDTO) {
-		return null;
+		int saveResult = memberService.save(memberDTO);
+		if(saveResult > 0) {
+			return "login";
+		}else {
+			return "save";
+		}
 		
 	}
 }
